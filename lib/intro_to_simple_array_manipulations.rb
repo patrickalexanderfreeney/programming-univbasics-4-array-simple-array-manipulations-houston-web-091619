ef245ccf144eacbd08453aa6bee38ce116286cf3
Patrick Alexander <patrick_alexander@mail.com>
2019-08-30 01:15:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,7 +6,7 @@ end
 
 def using_unshift(array, string)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  bouroughs_in_nyc.unshift
+  bouroughs_in_nyc.unshift(Staten Islasnd)
 end
 
 def using_pop
